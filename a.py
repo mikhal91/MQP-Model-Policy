1b894ba17517af481e81fd34b7496590ce7814e7
Michael Alicea <122935218+mikhal91@users.noreply.github.com>
2024-12-30 16:10:59 -0500

    Update a.py

--- a.py
+++ a.py
@@ -1,9 +1,6 @@
 import torch
-import torchvision
-import torchaudio
+
 
 print("PyTorch Version:", torch.__version__)
-print("Torchvision Version:", torchvision.__version__)
-print("Torchaudio Version:", torchaudio.__version__)
 print("CUDA Available:", torch.cuda.is_available())
 print("CUDA Version:", torch.version.cuda)
