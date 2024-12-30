56f2b67224894e395e2db37fc145a19800b56cf4
Michael Alicea <122935218+mikhal91@users.noreply.github.com>
2024-12-30 18:43:14 -0500

    testing

--- a.py
+++ a.py
@@ -1,6 +1,7 @@
 import torch
 
-
-print("PyTorch Version:", torch.__version__)
-print("CUDA Available:", torch.cuda.is_available())
-print("CUDA Version:", torch.version.cuda)
+print("PyTorch version:", torch.__version__)
+print("Is CUDA available?", torch.cuda.is_available())
+print("CUDA version:", torch.version.cuda)
+print("CUDA device count:", torch.cuda.device_count())
+print("Device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No CUDA Device")
